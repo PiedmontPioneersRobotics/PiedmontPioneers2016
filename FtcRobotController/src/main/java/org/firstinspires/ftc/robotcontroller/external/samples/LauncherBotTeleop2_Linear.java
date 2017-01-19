3bc4fa1e8756a9572925be4c0f3af7bef8f69551
henrylambert11 <henrygolfplayer@gmail.com>
2017-01-18 16:42:19 -0800

    Update LauncherBotTeleop2_Linear.java

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/LauncherBotTeleop2_Linear.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/LauncherBotTeleop2_Linear.java
@@ -1,23 +1,17 @@
 /*
 Copyright (c) 2016 Robert Atkinson
-
 All rights reserved.
-
 Redistribution and use in source and binary forms, with or without modification,
 are permitted (subject to the limitations in the disclaimer below) provided that
 the following conditions are met:
-
 Redistributions of source code must retain the above copyright notice, this list
 of conditions and the following disclaimer.
-
 Redistributions in binary form must reproduce the above copyright notice, this
 list of conditions and the following disclaimer in the documentation and/or
 other materials provided with the distribution.
-
 Neither the name of Robert Atkinson nor the names of his contributors may be used to
 endorse or promote products derived from this software without specific prior
 written permission.
-
 NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
 LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
@@ -66,9 +60,9 @@ public class LauncherBotTeleop2_Linear extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         double left = 0;
         double right = 0;
-        boolean waterMill = false;
+        boolean waterMill = true;
         double rubberbandspeed = 0;
-        boolean rubberBand = false;
+        boolean rubberBand = true;
         double waterMillSpeed = 0;
         double max;
         boolean touchsensor;
@@ -130,11 +124,11 @@ public class LauncherBotTeleop2_Linear extends LinearOpMode {
 
             else waterMillSpeed = 0.2;
 
-            if (watermillback> 0.5) {waterMillSpeed = -waterMillSpeed;}
+            if (watermillback> 0.5) {waterMillSpeed = 0.0;}
 
 
             if (rubberBand) rubberbandspeed = 0.0;
-            else rubberbandspeed = 1.0;
+            else rubberbandspeed = 0.5;
 
             if (rubberbandback> 0.5) {rubberbandspeed = -rubberbandspeed;}
 
